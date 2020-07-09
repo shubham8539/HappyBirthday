65ffe4769bcb5e1492992f915303a3b3cfdafb2f
Shubham <48403811+shubham8539@users.noreply.github.com>
2020-07-10 01:08:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "priya" && b=="nift")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
